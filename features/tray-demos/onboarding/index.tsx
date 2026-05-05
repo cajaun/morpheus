38b4a65920fd8a4560c24597f2dd3c061bca697a
cajaun <cajaun.campbell@gmail.com>
2026-05-05 08:11:37 -0500

    Slight changes to onboarding demo logic

--- features/tray-demos/onboarding/index.tsx
+++ features/tray-demos/onboarding/index.tsx
@@ -26,20 +26,27 @@ const OnboardingFooter = () => {
   );
 };
 
+const OnboardingHeader = ({ title }: { title: string }) => {
+  const { close, back, index } = useTrayFlow();
+
+  return (
+    <Tray.Header withSeparator>
+      <FlowHeader
+        step={index}
+        leftLabel={title}
+        shouldClose
+        onClose={close}
+        onBack={index > 0 ? back : undefined}
+      />
+    </Tray.Header>
+  );
+};
+
 const FirstStep = () => {
-  const { close, index } = useTrayFlow();
+
 
   return (
     <Tray.Body>
-      <Tray.Header withSeparator>
-        <FlowHeader
-          step={index}
-          leftLabel="Content One"
-          shouldClose
-          onClose={close}
-        />
-      </Tray.Header>
-
       <Tray.Section>
         <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
           This is a test title
@@ -59,20 +66,10 @@ const FirstStep = () => {
 };
 
 const SecondStep = () => {
-  const { close, back, index } = useTrayFlow();
+ 
 
   return (
     <Tray.Body>
-      <Tray.Header withSeparator>
-        <FlowHeader
-          step={index}
-          leftLabel="Content Two"
-          shouldClose
-          onBack={back}
-          onClose={close}
-        />
-      </Tray.Header>
-
       <Tray.Section>
         <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
           Different heading
@@ -109,34 +106,31 @@ const SecondStep = () => {
 };
 
 const ThirdStep = () => {
-  const { close, back, index } = useTrayFlow();
+
 
   return (
     <Tray.Pages>
-      <Tray.Pages.Header>
-        <Tray.Header withSeparator style={{ paddingHorizontal: 32 }}>
-          <FlowHeader
-            step={index}
-            leftLabel="Content Three"
-            shouldClose
-            onBack={back}
-            onClose={close}
-          />
-        </Tray.Header>
-      </Tray.Pages.Header>
+    
 
       <Tray.Page className="flex-1">
         <Tray.Body style={{ paddingHorizontal: 40, flex: 1 }}>
           <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
             Content Three A
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
-            Here's a lot more text. Lorem ipsum dolor amet. Lorem ipsum dolor amet.
-            Lorem ipsum dolor amet. Lorem ipsum dolor amet.
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
+            Here's a lot more text. Lorem ipsum dolor amet. Lorem ipsum dolor
+            amet. Lorem ipsum dolor amet. Lorem ipsum dolor amet.
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum hic
-            ut excepturi, aut eum obcaecati repudiandae! Aliquam expedita, minima.
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            hic ut excepturi, aut eum obcaecati repudiandae! Aliquam expedita,
+            minima.
           </Text>
         </Tray.Body>
       </Tray.Page>
@@ -146,11 +140,17 @@ const ThirdStep = () => {
           <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
             Content Three B
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
             This is the second fullscreen page. Keep tapping Continue to move
             through the built-in tray pages flow.
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
             Quisquam placeat praesentium at delectus veritatis, adipisci sint
             atque deserunt velit rem laborum neque minus doloribus.
           </Text>
@@ -162,12 +162,20 @@ const ThirdStep = () => {
           <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
             Content Three C
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
-            Final fullscreen page before returning to shell and going to Content Four.
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
+            Final fullscreen page before returning to shell and going to Content
+            Four.
           </Text>
-          <Text className="text-black font-sf-regular" style={trayDemoText.bodyLarge}>
+          <Text
+            className="text-black font-sf-regular"
+            style={trayDemoText.bodyLarge}
+          >
             This is some example test that spans over multiple lines bla bla bla
-            test test many words. This is a new sentence and we'll see how that fares too.
+            test test many words. This is a new sentence and we'll see how that
+            fares too.
           </Text>
         </Tray.Body>
       </Tray.Page>
@@ -176,21 +184,11 @@ const ThirdStep = () => {
 };
 
 const FourthStep = () => {
-  const { close, back, index } = useTrayFlow();
+
 
   return (
     <Tray.Body>
-      <Tray.Header withSeparator>
-        <FlowHeader
-          step={index}
-          leftLabel="Content Four"
-          shouldClose
-          onBack={back}
-          onClose={close}
-        />
-      </Tray.Header>
-
-      <Tray.Section >
+      <Tray.Section>
         <Text className="font-sf-bold" style={trayDemoText.bodyLarge}>
           Last Heading
         </Text>
@@ -224,30 +222,32 @@ const OnboardingExample = () => {
       {
         key: "content-one",
         content: <FirstStep />,
+        header: <OnboardingHeader title="Content One" />,
         options: { className: "bg-white" },
       },
       {
         key: "content-two",
         content: <SecondStep />,
+        header: <OnboardingHeader title="Content Two" />,
         options: { className: "bg-white" },
       },
       {
         key: "content-three",
         content: <ThirdStep />,
-        options: { className: "bg-white",
-             fullScreen: true,
+         header: <OnboardingHeader title="Content Three" />,
+        options: {
+          className: "bg-white",
+          fullScreen: true,
           fullScreenSafeAreaTop: true,
           fullScreenDraggable: false,
-          fullScreenCloseBehavior:
-           "returnToShell" ,
-         },
+          fullScreenCloseBehavior: "returnToShell",
+        },
       },
       {
         key: "content-four",
+         header: <OnboardingHeader title="Content Four" />,
         content: <FourthStep />,
-        options: { className: "bg-white",
-          
-         },
+        options: { className: "bg-white" },
       },
     ],
     [],
